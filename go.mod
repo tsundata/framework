9e1708e132ab35bf5f48a1b688d9a429b87b2c5d
sysatom <sysatom@gmail.com>
2020-10-09 11:15:13 +0800

    feat: base web handler

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/sysatom/framework
+
+go 1.14
+
+require web v0.0.0
+
+replace web => ./web
---
+++ web/web.go
@@ -0,0 +1,51 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	e.router[key] = handler
+}
+
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+func (e *Engine) PUT(pattern string, handler HandlerFunc) {
+	e.addRoute("PUT", pattern, handler)
+}
+
+func (e *Engine) DELETE(pattern string, handler HandlerFunc) {
+	e.addRoute("DELETE", pattern, handler)
+}
+
+func (e *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, e)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := e.router[key]; ok {
+		handler(w, req)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
